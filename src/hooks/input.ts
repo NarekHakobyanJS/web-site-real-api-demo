c641fbb73a43906c1e9f62b61a0ec2f1d6e43b60
NarekHakobyanJS <haobyan95narek@gmail.com>
2024-05-22 19:10:41 +0400

    s

--- src/hooks/input.ts
+++ src/hooks/input.ts
@@ -5,6 +5,9 @@ interface InputReturn {
     onChange: (event: ChangeEvent<HTMLInputElement>) => void
 }
 export function useInput (initalValue = ""):InputReturn {
+    //
+    //
+    
     const [value, setValue] = useState(initalValue)
     const onChange = (event: ChangeEvent<HTMLInputElement>) => {
         setValue(event.target.value)
